32f7a31ad915c8c31b6c5389537d7b2408f1744f
Yazid Bounab <bounab.yazid@gmail.com>
2019-04-13 12:03:29 +0100

    Add files via upload

---
+++ LoadXML.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 08:41:28 2019
+
+@author: polo
+"""
+import pandas as pd 
+import xml.etree.ElementTree as et 
+
+xtree = et.parse("Webscope_L24/ydata-search-query-log-to-entities-v1_0.xml")
+xroot = xtree.getroot()
+
+
+Sessions = []
+#for child in xroot:
+children = xroot.getchildren()
+for child in children:
+    Session = {'id':'','numqueries':'','queries':[]}
+    
+    Session['id'] = child.attrib.get("id")
+    Session['numqueries'] = child.attrib.get("numqueries")
+    
+    #Session['query'] = child[0].find('text').text
+    #print (et.dump(child))
+    for node in child:
+        Attributes = {'adult':'', 'ambiguous':'', 'assessor':'', 'cannot-judge':'', 'navigational':'', 'no-wp':'', 'non-english':'', 'quote-question':'', 'starttime':''}
+        Query = {'text':'','attribues':Attributes,'annotations':[]}
+        Query['text'] = node.find('text').text if node is not None else None
+       
+        for key in Attributes.keys():
+            Query['attribues'][key] = node.attrib.get(key)
+            
+        for annot in node.findall('annotation'):
+            annotation = {'main':'','span':''}
+            annotation['main'] = annot.attrib.get("main")
+            annotation['span'] = annot.find('span').text
+            
+            if annot.find('target'):
+               Target = {'wiki-id':'','link':''}
+               Target['id'] = annot.find('target').attrib.get('wiki-id')
+               Target['link'] = annot.find('target').text
+               
+               annotation['target'] = Target
+            Query['annotations'].append(annotation)
+        
+        Session['queries'].append(Query)
+    #break    
+    Sessions.append (Session)
