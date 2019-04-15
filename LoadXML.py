53fdf3b35c65ca3261d8dbdbc5e2f773cb4704a2
Yazid Bounab <bounab.yazid@gmail.com>
2019-04-15 15:26:04 +0100

    Add files via upload

--- LoadXML.py
+++ LoadXML.py
@@ -2,52 +2,69 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Apr 13 08:41:28 2019
-@author: Yazid Bounab
+https://robertopreste.com/blog/parse-xml-into-dataframe
+@author: Yazid BOUNAB
 """
+import pickle
+
 import pandas as pd 
 import xml.etree.ElementTree as et 
 
-xtree = et.parse("Webscope_L24/ydata-search-query-log-to-entities-v1_0.xml")
-xroot = xtree.getroot()
 
-Sessions = []
+def SaveListToFile(mylist,filename):
+    with open(filename, 'wb') as filehandle:
+         pickle.dump(mylist, filehandle)
+         
+def LoadFileToList(filename):
+    mylist = []
+    with open(filename, 'rb') as filehandle:  
+         mylist = pickle.load(filehandle)
+    return mylist
 
-IDS = []
-Queries = []
+def LoadXML_to_Sessions():
+    xtree = et.parse("Webscope_L24/ydata-search-query-log-to-entities-v1_0.xml")
+    xroot = xtree.getroot()
 
-children = xroot.getchildren()
-for child in children:
-    Session = {'id':'','numqueries':'','queries':[]}
+    Sessions = []
+    #for child in xroot:
+    Queries = []
+    children = xroot.getchildren()
+    for child in children:
+        Session = {'id':'','numqueries':'','queries':[]}
     
-    Session['id'] = child.attrib.get("id")
-    Session['numqueries'] = child.attrib.get("numqueries")
+        Session['id'] = child.attrib.get("id")
+        Session['numqueries'] = child.attrib.get("numqueries")
     
-    IDS.append(Session['id'])
-    
-    for node in child:
-        Attributes = {'adult':'', 'ambiguous':'', 'assessor':'', 'cannot-judge':'', 'navigational':'', 'no-wp':'', 'non-english':'', 'quote-question':'', 'starttime':''}
-        Query = {'text':'','attribues':Attributes,'annotations':[]}
-        Query['text'] = node.find('text').text if node is not None else None
+        #Session['query'] = child[0].find('text').text
+        #print (et.dump(child))
+        for node in child:
+            Attributes = {'adult':'', 'ambiguous':'', 'assessor':'', 'cannot-judge':'', 'navigational':'', 'no-wp':'', 'non-english':'', 'quote-question':'', 'starttime':''}
+            Query = {'text':'','attribues':Attributes,'annotations':[]}
+            Query['text'] = node.find('text').text if node is not None else None
         
-        Queries.append(Query['text'])
+            Queries.append(Query['text'])
         
-        for key in Attributes.keys():
-            Query['attribues'][key] = node.attrib.get(key)
+            for key in Attributes.keys():
+                Query['attribues'][key] = node.attrib.get(key)
             
-        for annot in node.findall('annotation'):
-            annotation = {'main':'','span':''}
-            annotation['main'] = annot.attrib.get("main")
-            annotation['span'] = annot.find('span').text
+            for annot in node.findall('annotation'):
+                annotation = {'main':'','span':''}
+                annotation['main'] = annot.attrib.get("main")
+                annotation['span'] = annot.find('span').text
             
-            if annot.find('target'):
-               Target = {'wiki-id':'','link':''}
-               Target['id'] = annot.find('target').attrib.get('wiki-id')
-               Target['link'] = annot.find('target').text
+                if annot.find('target'):
+                   Target = {'wiki-id':'','link':''}
+                   Target['id'] = annot.find('target').attrib.get('wiki-id')
+                   Target['link'] = annot.find('target').text
                
-               annotation['target'] = Target
-            Query['annotations'].append(annotation)
-        Session['queries'].append(Query)
-    Sessions.append (Session)
-    
-IDS = list(set(IDS))
-Queries = list(set(Queries))
+                   annotation['target'] = Target
+                Query['annotations'].append(annotation)
+        
+            Session['queries'].append(Query)
+        Sessions.append (Session)
+    return Sessions,Queries
+
+#Sessions,Queries = LoadXML_to_Sessions()
+
+#SaveListToFile(Queries,'Data/Queries.pkl')
+#SaveListToFile(Sessions,'Data/Sessions.pkl')
--- SNA_NER.py
+++ SNA_NER.py
@@ -5,12 +5,17 @@ Created on Thu Apr 11 09:44:42 2019
 
 @author: polo
 """
+from LoadXML import LoadXML_to_Sessions
 
 from nltk.tag.stanford import StanfordNERTagger
 from nltk.tokenize import word_tokenize
 
+tagger = StanfordNERTagger('/home/polo/Downloads/stanford-ner-2018-02-27/classifiers/english.all.3class.distsim.crf.ser.gz',
+                           '/home/polo/Downloads/stanford-ner-2018-02-27/stanford-ner.jar', encoding='utf-8')
+
 def formatted_entities(classified_paragraphs_list):
     entities = {'persons': list(), 'organizations': list(), 'locations': list()}
+               # 'Money': list(), 'Percent': list(), 'Date': list(),'Time': list()}
 
     for classified_paragraph in classified_paragraphs_list:
         for entry in classified_paragraph:
@@ -25,29 +30,38 @@ def formatted_entities(classified_paragraphs_list):
 
             elif entry_type == 'LOCATION':
                 entities['locations'].append(entry_value)
-
+                
     return entities
 
+def NER_Entities(classified_Text_list,Queries):
+    NER = []
+    NE = ['PERSON', 'ORGANIZATION', 'LOCATION']
 
-tagger = StanfordNERTagger('/home/polo/Downloads/stanford-ner-2018-02-27/classifiers/english.all.3class.distsim.crf.ser.gz',
-               '/home/polo/Downloads/stanford-ner-2018-02-27/stanford-ner.jar',
-               encoding='utf-8')
+    for i in range(0,len(classified_Text_list)):   
+        L = []
+        for entry in classified_Text_list[i]:
+            entry_value = entry[0]
+            entry_type = entry[1]
+
+            if entry_type in NE:
+               L.append(entry_value)
+
+        NER.append({Queries[i]:L})
+        
+    return NER
+
+Sessions,Queries = LoadXML_to_Sessions()
+
+tokenized_Queries = []
+
+for Query in Queries:
+    tokenized_Queries.append(word_tokenize(Query))
+    
+classified_Text_list = tagger.tag_sents(tokenized_Queries)
+
+NER = NER_Entities(classified_Text_list,Queries)
 
 
-paragraphs = ['NEWYOURK',
-            'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.',
-            "Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services. Its hardware products include the iPhone smartphone, the iPad tablet computer, the Mac personal computer, the iPod portable media player, the Apple Watch smartwatch, and the Apple TV digital media player. Apple's consumer software includes the OS X and iOS operating systems, the iTunes media player, the Safari web browser, and the iLife and iWork creativity and productivity suites. Its online services include the iTunes Store, the iOS App Store and Mac App Store, and iCloud. Apple was founded by Steve Jobs, Steve Wozniak, and Ronald Wayne on April 1, 1976, to develop and sell personal computers. It was incorporated as Apple Computer, Inc. on January 3, 1977, and was renamed as Apple Inc. on January 9, 2007, to reflect its shifted focus toward consumer electronics. Apple (NASDAQ: AAPL ) joined the Dow Jones Industrial Average on March 19, 2015.",
-            "Samuel Patterson Smyth \"Sam\" Pollock, OC, CQ (December 25, 1925 – August 15, 2007) was sports executive who was general manager of the National Hockey League's Montreal Canadiens for 14 years where they won 9 Stanley Cups. Pollock also served as Chairman and CEO of the Toronto Blue Jays baseball club.",
-        'Yazid is PhD student university of Oulu']
+    
+#formatted_result = formatted_entities(classified_Text_list)
 
-#paragraphs = ['While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.' ]
-tokenized_paragraphs = list()
-
-for text in paragraphs:
-    tokenized_paragraphs.append(word_tokenize(text))
-
-classified_paragraphs_list = tagger.tag_sents(tokenized_paragraphs)
-
-
-formatted_result = formatted_entities(classified_paragraphs_list)
-print(formatted_result)
